31c23d32149f2b00a738ecb07d2bb2e075e85ca7
letthoung <thanhtung3496@gmail.com>
2020-06-26 13:43:29 -0400

    Added error handling for both server and client side

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,18 +1,21 @@
 import axios, { AxiosResponse } from 'axios';
 import { IActivity } from '../models/activity';
-
+import { history } from '../..';
 axios.defaults.baseURL = 'http://localhost:5000/api';
+
+axios.interceptors.response.use(undefined, error => {
+    if (error.response.status === 404) {
+        history.push('/notfound');
+    }
+})
 
 const responseBody = (response: AxiosResponse) => response.data;
 
-const sleep = (ms: number) => (response: AxiosResponse) => 
-    new Promise<AxiosResponse>(resolve => setTimeout(() => resolve(response), ms));
-
 const requests = {
-    get: (url: string) => axios.get(url).then(sleep(1000)).then(responseBody),
-    post: (url: string, body: {}) => axios.post(url, body).then(sleep(1000)).then(responseBody),
-    put: (url: string, body: {}) => axios.put(url, body).then(sleep(1000)).then(responseBody),
-    del: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody) 
+    get: (url: string) => axios.get(url).then(responseBody),
+    post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
+    put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
+    del: (url: string) => axios.delete(url).then(responseBody) 
 };
 
 const Activities = {
--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -13,7 +13,7 @@ interface DetailParams {
   id: string;
 }
 
-const ActivityDetails: React.FC<RouteComponentProps<DetailParams>> = ({ match }) => {
+const ActivityDetails: React.FC<RouteComponentProps<DetailParams>> = ({ match, history }) => {
   const activityStore = useContext(ActivityStore);
   const {
     activity,
@@ -23,9 +23,13 @@ const ActivityDetails: React.FC<RouteComponentProps<DetailParams>> = ({ match })
 
   useEffect(() => {
     loadActivity(match.params.id);
-  }, [loadActivity, match.params.id]);
+  }, [loadActivity, match.params.id, history]);
 
-  if (loadingInitial || !activity) return <LoadingComponent content='Loading activity...' />
+  if (loadingInitial) 
+    return <LoadingComponent content='Loading activity...' />
+
+  if (!activity)
+    return <h2>Activity Not Found</h2>
 
   return (
     <Grid>
