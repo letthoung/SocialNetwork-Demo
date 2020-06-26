27e45773c2c17e935c5198d7b0bbe2e889d8844a
letthoung <thanhtung3496@gmail.com>
2020-06-26 14:08:48 -0400

    Added an error toast to notify when connection error occurs, especially when the server is not running

--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -26,6 +26,7 @@ namespace Application.Activities
 
             public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
             {
+                throw new Exception("Nothing");
                 var activity = await _context.Activities.FindAsync(request.Id);
                 if (activity == null)
                     throw new RestException(HttpStatusCode.NotFound, new {activity = "Not Found"});
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './app/layout/styles.css';
+import 'react-toastify/dist/ReactToastify.min.css';
 import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
 import ScrollToTop from './app/layout/ScrollToTop';
